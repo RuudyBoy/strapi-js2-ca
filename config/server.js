087856a1ea09cb21b4839322b38296835e77b7dc
RuudyBoy <73653923+RuudyBoy@users.noreply.github.com>
2021-12-13 11:07:55 +0100

    ø

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 8060),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '23287cd0b2aea4af40a4349f4540ab6e'),
